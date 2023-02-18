4d96df88addbaa39ea967214b7ea1462406d883c
abdullah07 <abdullaherol07@gmail.com>
2023-02-18 11:55:13 -0500

    fixex module

--- metrics.server.tf
+++
@@ -1,9 +0,0 @@
-module metrics-server {
-    source = "./module"
-    deployment_name = "metrics-server"
-    namespace  = "kube-system"
-    repository = "https://prometheus-community.github.io/helm-charts"
-    chart      = "kube-state-metrics"
-}
-
- 
--- modules/terraform-helm/main.tf
+++ modules/terraform-helm/main.tf
@@ -1,10 +1,3 @@
-resource "helm_release" "metrics-server" {
-  name       = var.deployment_name
-  namespace  = var.namespace
-  repository = var.repository
-  chart      = var.chart
-  wait       = false
-}
 resource "helm_release" "prometheus" {
   name       = var.deployment_name
   namespace  = var.namespace
